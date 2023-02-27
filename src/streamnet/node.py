3efbf64528c23536554ea50d7dd7c397417b5df8
anglyan <angel.yanguas@gmail.com>
2023-02-26 19:21:12 -0600

    renamed elements by nodes

---
+++ src/streamnet/node.py
@@ -0,0 +1,21 @@
+#Copyright Argonne 2022. See LICENSE.md for details.
+
+
+class Element:
+
+    def __init__(self, method, default, n_out=1):
+
+        self.object = method
+        self.out = default
+        self.n_out = n_out
+
+    def run_method(self, name, *args):
+        m = getattr(self.object, name)
+        m(*args)
+        
+    def __call__(self, *args):
+        if self.n_out == 1:
+            self.out = [self.object(*args)]
+        else:
+            self.out = self.object(*args)
+
--- src/streamnet/streamnet.py
+++ src/streamnet/streamnet.py
@@ -2,7 +2,7 @@
 
 """
 
-An element has the following interface:
+An node has the following interface:
 
 - It needs to be callable
 - It has to implement out, which stores the result of the last
@@ -10,29 +10,13 @@ An element has the following interface:
 
 """
 
-class Element:
-
-    def __init__(self, el_object, default, n_out=1):
-
-        self.object = el_object
-        self.out = default
-        self.n_out = n_out
-
-    def run_method(self, name, *args):
-        m = getattr(self.object, name)
-        m(*args)
-        
-    def __call__(self, *args):
-        if self.n_out == 1:
-            self.out = [self.object(*args)]
-        else:
-            self.out = self.object(*args)
-
-
 class StreamNet:
 
     """
     Implement a streamnet
+
+    Args:
+        return_values: bool, streamnet returns value after call
 
 
     """
@@ -42,19 +26,19 @@ class StreamNet:
         self._iports = []
         self._iports_dict = {}
         self._oports = []
-        self._elements = {}
+        self._nodes = {}
         self._el_name_dict = {}
         self._el_in = {}
         self._el_out = {}
         self.return_values = return_values
 
 
-    def add_element(self, name, el, n_in=0, n_out=1):
-        """Adds an element to the streamnet
+    def add_node(self, name, el, n_in=0, n_out=1):
+        """Adds an node to the streamnet
 
         Args:
-            name : the name of the element
-            el : an element
+            name : the name of the node
+            el : an node
             n_in : number of inputs
             n_out : number of outputs
         
@@ -62,15 +46,15 @@ class StreamNet:
             ValueError: User tries to reuse an existing name
         """
 
-        if name in self._elements.keys():
-            raise ValueError("Element {} already defined".format(name))
+        if name in self._nodes.keys():
+            raise ValueError("node {} already defined".format(name))
 
-        self._elements[name] = el
+        self._nodes[name] = el
         self._el_in[name] = [None for i in range(n_in)]
         self._el_out[name] = n_out
 
-    def get_element_names(self):
-        return self._elements.keys()[:]
+    def get_node_names(self):
+        return self._nodes.keys()[:]
 
     def get_input_names(self):
         return self._iports[:]
@@ -95,37 +79,37 @@ class StreamNet:
         
         Args:
             name: name of the layer
-            n: index of the element output to be returned (optional, default 1)
+            n: index of the node output to be returned (optional, default 1)
              
         """
-        if name in self._elements.keys():
+        if name in self._nodes.keys():
             if self._el_out[name] > n:
-                raise ValueError("Element {} has fewer than {} outputs".format(name, n))
+                raise ValueError("node {} has fewer than {} outputs".format(name, n))
             else:
                 self._oports.append((name, n))
         elif name in self._iports:
             self._oports.append((name,))
         else:
-            raise ValueError("Element or Input {} not found".format(name))
+            raise ValueError("node or Input {} not found".format(name))
 
     def set_el_inputs(self, name, *args):
-        if name not in self._elements.keys():
-            raise ValueError("Element or Input {} not found".format(name))
+        if name not in self._nodes.keys():
+            raise ValueError("node or Input {} not found".format(name))
         arg_list = []
         for arg in args:
             if isinstance(arg, str):
                 if not self.name_exists(arg):
-                    raise ValueError("Element or Input {} not found".format(arg))
+                    raise ValueError("node or Input {} not found".format(arg))
                 arg_list.append((arg,))
             else:
-                if arg[0] not in self._elements.keys():
-                    raise ValueError("Element {} not found".format(arg[0]))
+                if arg[0] not in self._nodes.keys():
+                    raise ValueError("node {} not found".format(arg[0]))
                 arg_list.append(arg)
         self._el_in[name] = arg_list
 
     def set_el_input(self, el_name, n_in, name, n_out=None):
-        if name not in self._elements.keys():
-            raise ValueError("Element or Input {} not found".format(name))
+        if name not in self._nodes.keys():
+            raise ValueError("node or Input {} not found".format(name))
 
         if n_out is None:
             self._el_in[el_name][n_in] = (name,)
@@ -133,12 +117,12 @@ class StreamNet:
             self._el_in[el_name][n_in] = (name, n_out)
 
     def name_exists(self, name):
-        return (name in self._elements.keys()) or (name in self._iports)
+        return (name in self._nodes.keys()) or (name in self._iports)
     
     def add_el_input(self, el_name, name, n_out=None):
     
         if not self.name_exists(name):
-            raise ValueError("Element or Input {} not found".format(name))
+            raise ValueError("node or Input {} not found".format(name))
 
         if n_out is None:
             self._el_in[el_name].append((name,))
@@ -146,13 +130,13 @@ class StreamNet:
             self._el_in[el_name].append((name, n_out))
 
     def broadcast(self, method_name, *method_args):
-        for _, el in self._elements.items():
+        for _, el in self._nodes.items():
             f = getattr(el, method_name)
             f(*method_args)
 
     def __call__(self, *args):
         input_dict = {}
-        for name, el in self._elements.items():
+        for name, el in self._nodes.items():
             input_list = []
             for el_input in self._el_in[name]:
                 inp_name = el_input[0]
@@ -166,14 +150,14 @@ class StreamNet:
 
                     if self._el_out[inp_name] == 1:
                         if n_out == 1:
-                            input_list.append(self._elements[inp_name].out)
+                            input_list.append(self._nodes[inp_name].out)
                         else:
                             raise ValueError("{} is greater than the number of outputs".format(n_out))
                     else:
-                        input_list.append(self._elements[inp_name].out[n_out])
+                        input_list.append(self._nodes[inp_name].out[n_out])
             input_dict[name] = input_list
         
-        for name, el in self._elements.items():
+        for name, el in self._nodes.items():
             el(*input_dict[name])
         
         self.out = []
@@ -184,13 +168,10 @@ class StreamNet:
             else:
                 name, nout = op
                 if self._el_out[name] == 1:
-                    self.out.append(self._elements[name].out)
+                    self.out.append(self._nodes[name].out)
                 else:
-                    self.out.append(self._element[name.out[nout-1]])
+                    self.out.append(self._nodes[name].out[nout-1])
         
-#        if len(self._oports) == 1:
-#            self.out = self.out[0]
-
         if self.return_values:
             return self.out
 
